31d3992a23cffdea57c62d7aa8e9b2225f8a95ea
sskelkar <skelkar@equalexperts.com>
2019-01-09 00:21:26 +0530

    removed @EnableTransactionManagement annotation

--- src/test/java/example/TestConfiguration.java
+++ src/test/java/example/TestConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.testcontainers.containers.MySQLContainer;
 
 import javax.sql.DataSource;
@@ -18,7 +17,6 @@ import java.util.Properties;
 
 @Configuration
 @ComponentScan({"example"})
-@EnableTransactionManagement
 public class TestConfiguration {
 
   @Bean
